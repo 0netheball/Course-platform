92cbb347307f04f7a4100e5a2fff02998df8f38e
Ontheball <bravelivesforever@gmail.com>
2025-06-08 19:16:44 +0300

    I did authentification pages! updated middleware out of clerkdocs

--- my-app/app/(dashboard)/(routes)/page.tsx
+++ my-app/app/(dashboard)/(routes)/page.tsx
@@ -2,10 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="p-4">
-      {/* <UserButton afterSign>
-        
-      </UserButton> */}
+    <div>
+      <UserButton afterSignOutUrl="/"></UserButton>
     </div>
   );
 }
